c3ec4eb4873c734219cd5ea780e455b240ad2f80
Carla Caroline <carla.caroline@mobyan.com.br>
2024-02-14 22:01:49 -0300

    inicio atividade

---
+++ scripts/index.js
@@ -0,0 +1,22 @@
+let nome = prompt ('insira seu nome')
+let sobrenome = prompt ('insira seu sobrenome')
+
+
+const nomeCompleto = '${nome} ${sobrenome}'
+alert ('digite aqui seu nome:' + parseInt (nome)+ parseInt (sobrenome))
+
+console.log (nomeCompleto)
+
+
+
+let numeroInserido = prompt ('inserir numero')
+let numeroInserido2 = prompt ('insira outro numero')
+
+console.log(parseInt(numeroInserido)+ parseInt (numeroInserido2))
+
+alert ('o produto é:' + parseInt (numeroInserido) * parseInt (numeroInserido2))
+
+
+
+
+console.log(numeroInserido)
